93737c95dc759495bdcf62c7cd2804b75be29f5d
Sergei Makarov <smakarov@intesigroup.local>
2022-09-01 12:04:49 +0200

    Start of the assignment

---
+++ src/main/java/guru/springframework/services/RecipeServiceImpl.java
@@ -0,0 +1,15 @@
+package guru.springframework.services;
+
+import java.util.Set;
+
+import guru.springframework.domain.Recipe;
+
+public class RecipeServiceImpl implements RecipeService {
+
+	@Override
+	public Set<Recipe> getRecipes() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
